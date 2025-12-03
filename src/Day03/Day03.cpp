19cefc989e624780fbe7ecdc3e2c91164573ccdd
Michael Braedley <michael.braedley@cartenav.com>
2025-12-03 10:01:24 -0400

    Day 03 complete

--- src/Day03/Day03.cpp
+++ src/Day03/Day03.cpp
@@ -12,12 +12,69 @@ int main()
 
 	auto input = utils::ReadInput( "../Day03_input.txt" );
 
-	//utils::PrintResult(val, startTime);
+	std::uint64_t val = 0;
+
+	for( const auto& bank : input )
+	{
+		int digit1 = 0;
+		std::size_t pos = 0;
+
+		for( std::size_t i = 0; i < bank.size() - 1; i++ )
+		{
+			if( bank[i] - '0' > digit1 )
+			{
+				digit1 = bank[i] - '0';
+				pos = i;
+			}
+		}
+
+		int digit0 = 0;
+
+		for( std::size_t i = pos + 1; i < bank.size(); i++ )
+		{
+			if( bank[i] - '0' > digit0 )
+			{
+				digit0 = bank[i] - '0';
+			}
+		}
+
+		val += digit1 * 10 + digit0;
+	}
+
+	utils::PrintResult(val, startTime);
 
 	//Part 2:
 
+	val = 0;
 
-	//utils::PrintResult(val, startTime);
+	for( const auto& bank : input )
+	{
+		std::array<std::uint8_t, 12> joltage{};
+		std::size_t pos = 0;
+		for( std::size_t i = 0; i < 12; i++ )
+		{
+			for( std::size_t j = pos; j < bank.size() - 11 + i; j++ )
+			{
+				if( bank[j] - '0' > joltage[i] )
+				{
+					joltage[i] = bank[j] - '0';
+					pos = j + 1;
+				}
+			}
+		}
+
+		std::uint64_t j = 0;
+		for( auto i : joltage )
+		{
+			j = j * 10 + i;
+		}
+
+		//utils::PrintResult( j, startTime );
+
+		val += j;
+	}
+
+	utils::PrintResult(val, startTime);
 
 	return 0;
 }
