953a9f2ba65b39d34143abebee8151980f327148
Michael Braedley <mike.braedley@gmail.com>
2025-12-06 21:02:53 -0400

    Day 06 complete

--- src/Day06/Day06.cpp
+++ src/Day06/Day06.cpp
@@ -12,12 +12,79 @@ int main()
 
 	auto input = utils::ReadInput( "../Day06_input.txt" );
 
-	//utils::PrintResult(val, startTime);
+	std::vector<std::string> operations = utils::Tokenize(input.back(), ' ');
+	input.pop_back();
+
+	std::vector<std::vector<std::uint64_t>> vals;
+	for (const auto& line : input)
+	{
+		vals.push_back(
+			std::views::all(utils::Tokenize(line, ' ')) |
+			std::views::transform([](const auto& v) { return std::stoull(v); }) |
+			std::ranges::to<std::vector<std::uint64_t>>());
+	}
+
+	vals = utils::Transpose(vals);
+
+	std::uint64_t val = 0;
+	for (std::size_t i = 0; i < vals.size(); i++)
+	{
+		if (operations[i] == "+")
+		{
+			val += *std::ranges::fold_left_first(vals[i], std::plus());
+		}
+		else
+		{
+			val += *std::ranges::fold_left_first(vals[i], std::multiplies());
+		}
+	}
+
+	utils::PrintResult(val, startTime);
 
 	//Part 2:
 
+	auto nums = utils::Transpose(input) | std::ranges::to<std::deque<std::string>>();
+	val = 0;
 
-	//utils::PrintResult(val, startTime);
+	for (std::size_t i = 0; i < operations.size(); i++)
+	{
+		if (operations[i] == "+")
+		{
+			std::uint64_t inter = 0;
+			while (!nums.empty() && !std::ranges::all_of(std::views::all(nums.front()), [](const auto& c) {return c == ' '; }))
+			{
+				inter += std::stoull(nums.front());
+				nums.pop_front();
+			}
+
+			if (!nums.empty())
+			{
+				//pop the empty value
+				nums.pop_front();
+			}
+
+			val += inter;
+		}
+		else
+		{
+			std::uint64_t inter = 1;
+			while (!nums.empty() && !std::ranges::all_of(std::views::all(nums.front()), [](const auto& c) {return c == ' '; }))
+			{
+				inter *= std::stoull(nums.front());
+				nums.pop_front();
+			}
+
+			if (!nums.empty())
+			{
+				//pop the empty value
+				nums.pop_front();
+			}
+
+			val += inter;
+		}
+	}
+
+	utils::PrintResult(val, startTime);
 
 	return 0;
 }
