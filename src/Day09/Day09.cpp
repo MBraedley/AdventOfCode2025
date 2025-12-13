39e3c221de237734d779f80d13e1b81ea9569004
Michael Braedley <mike.braedley@gmail.com>
2025-12-13 19:39:58 -0400

    Day 09 part 1, part 2 still has edge cases that need to be resolved

--- src/Day09/Day09.cpp
+++ src/Day09/Day09.cpp
@@ -4,24 +4,71 @@
 #include "Day09.h"
 #include "Utils.h"
 
-#include <optional>
-#include <deque>
-#include <list>
+#include <set>
+#include <map>
 #include <ranges>
-#include <cassert>
 
 int main()
 {
 	auto startTime = std::chrono::system_clock::now();
 
-	auto input = utils::ReadInput( "../Day09_input.txt" );
+	std::filesystem::path inputFile("../Day09_input.txt");
 
-	//utils::PrintResult(val, startTime);
+	std::ifstream istrm(inputFile);
+
+	std::multimap<std::uint64_t, std::pair<utils::Pos, utils::Pos>> areas;
+	std::set<utils::Pos> tiles;
+
+	for (utils::Pos p; istrm >> p; )
+	{
+		for (const utils::Pos tile : tiles)
+		{
+			auto diff = p - tile;
+			std::uint64_t area = (std::abs(diff.X) + 1) * (std::abs(diff.Y) + 1);
+
+			areas.emplace(area, std::make_pair(p, tile));
+		}
+
+		tiles.insert(p);
+	}
+
+	utils::PrintResult(areas.rbegin()->first, startTime);
 
 	//Part 2:
 
+	auto isWithin = [](const utils::Pos& p, const std::pair<utils::Pos, utils::Pos>& corners) -> bool
+		{
+			return p.X > std::min(corners.first.X, corners.second.X)
+				&& p.X < std::max(corners.first.X, corners.second.X)
+				&& p.Y > std::min(corners.first.Y, corners.second.Y)
+				&& p.Y < std::max(corners.first.Y, corners.second.Y);
+		};
 
-	//utils::PrintResult(val, startTime);
+	auto isOnEdge = [](const utils::Pos& p, const std::pair<utils::Pos, utils::Pos>& corners) -> bool
+		{
+			return (p.X == std::min(corners.first.X, corners.second.X)
+					&& p.Y >= std::min(corners.first.Y, corners.second.Y)
+					&& p.Y <= std::max(corners.first.Y, corners.second.Y))
+				|| (p.X == std::max(corners.first.X, corners.second.X)
+					&& p.Y >= std::min(corners.first.Y, corners.second.Y)
+					&& p.Y <= std::max(corners.first.Y, corners.second.Y))
+				|| (p.Y == std::min(corners.first.Y, corners.second.Y)
+					&& p.X >= std::min(corners.first.X, corners.second.X)
+					&& p.X <= std::max(corners.first.X, corners.second.X))
+				|| (p.Y == std::max(corners.first.Y, corners.second.Y)
+					&& p.X >= std::min(corners.first.X, corners.second.X)
+					&& p.X <= std::max(corners.first.X, corners.second.X));
+		};
+
+	for (auto iter = areas.rbegin(); iter != areas.rend(); iter++)
+	{
+		if (std::ranges::none_of(tiles, [=](const utils::Pos& p) { return isWithin(p, iter->second); })
+			&& std::ranges::count_if(tiles, [=](const utils::Pos& p) { return isOnEdge(p, iter->second); }) % 2 == 0)
+		{
+			utils::PrintResult(iter->first, startTime);
+			break;
+		}
+	}
 
 	return 0;
 }
