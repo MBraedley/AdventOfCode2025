db3467ee308e0020c7a86359ac99c5117d3a9123
Michael Braedley <mike.braedley@gmail.com>
2026-01-02 11:40:55 -0400

    start of day 10 (parse most of the input)

--- src/Day10/Day10.cpp
+++ src/Day10/Day10.cpp
@@ -5,12 +5,54 @@
 #include "Utils.h"
 
 #include <ranges>
+#include <bitset>
+
+struct MachineConsole
+{
+	std::bitset<10> lights;
+	std::vector<std::bitset<10>> buttons;
+	std::vector<std::uint16_t> joltage;
+};
+
 
 int main()
 {
 	auto startTime = std::chrono::system_clock::now();
 
-	auto input = utils::ReadInput( "../Day10_input.txt" );
+	std::filesystem::path inputFile("../Day10_input.txt");
+
+	std::ifstream instrm(inputFile);
+	std::string line;
+
+	std::vector<MachineConsole> machines;
+
+	while (std::getline(instrm, line))
+	{
+		auto tokens = utils::Tokenize(line, ' ') | std::ranges::to<std::deque<std::string>>();
+
+		std::string lights = tokens.front();
+		tokens.pop_front();
+
+		MachineConsole mc;
+
+		std::bitset<10> bs(std::ranges::reverse_view(lights) | std::ranges::to<std::string>(), 1, lights.size() - 2, '.', '#');
+
+		mc.lights = bs;
+
+		std::ranges::for_each(
+			std::views::all(tokens) | std::views::take_while([](const std::string& s) { return s.starts_with("("); }),
+			[&](const std::string& s)
+			{
+				bs.reset();
+				std::ranges::for_each(
+					std::views::all(s) | std::views::filter([](const char& c) { return c >= '0' && c <= '9'; }) | std::views::transform([](const char& c) { return c - '0'; }),
+					[&](const auto& i)
+					{
+						bs.set(i);
+					});
+				mc.buttons.push_back(bs);
+			});
+	}
 
 	//utils::PrintResult(val, startTime);
 
